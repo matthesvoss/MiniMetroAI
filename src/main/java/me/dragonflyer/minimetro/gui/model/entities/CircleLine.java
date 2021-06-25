8cb47e464b4bdb01b18bf3f376bb80ab57188930
Matthes Voß <matthesvoss7@gmail.com>
2021-06-25 23:30:25 +0200

    implement CircleLine

--- src/main/java/me/dragonflyer/minimetro/gui/model/entities/CircleLine.java
+++ src/main/java/me/dragonflyer/minimetro/gui/model/entities/CircleLine.java
@@ -1,25 +1,58 @@
 package me.dragonflyer.minimetro.gui.model.entities;
 
-import java.awt.*;
+import me.dragonflyer.minimetro.gui.model.exceptions.NoFreePlatformException;
 
-public class CircleLine extends Line {//TODO
+import java.util.List;
 
-    private Direction dir;
+public class CircleLine extends Line {
 
-    public CircleLine(Station[] stations) {
+    private Station firstAndLastStation;
+
+    public CircleLine(List<Station> stations) {
         super(stations);
+        this.firstAndLastStation = super.getFirstStation();
     }
 
-    public Direction getDirection() {
-        return dir;
+    @Override
+    public void calculateLineSections() throws NoFreePlatformException {
+        super.calculateLineSections();
+        super.calculateLineSection(super.getLastStation(), super.getFirstStation());
     }
 
-    public void setDirection(Direction dir) {
-        this.dir = dir;
+    @Override
+    public Station getLastStation() {
+        return firstAndLastStation;
     }
 
-    public enum Direction {
-        FORWARDS, BACKWARDS
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CircleLine other = (CircleLine) o;
+        List<Station> stations = getStations();
+        List<Station> otherStations = other.getStations();
+        int stationsSize = stations.size();
+        int otherStationsSize = otherStations.size();
+        if (otherStationsSize != stationsSize) {
+            return false;
+        }
+        // check if otherStations is rotated version of stations
+        int offset = otherStations.indexOf(firstAndLastStation);
+        if (offset == -1) {
+            return false;
+        }
+        for (int i = 1; i < stationsSize; i++) {
+            Station station = stations.get(i);
+            Station otherStation = otherStations.get((i + offset) % otherStationsSize);
+            if (!station.equals(otherStation)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
